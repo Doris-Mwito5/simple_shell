8196d5552c60240e7473608e0619814af1d00e5a
kelvindaudi <kelvindaudi98@gmail.com>
2023-11-15 05:35:27 -0800

    corr

--- env.c
+++ env.c
@@ -1,7 +1,8 @@
 #include "shell.h"
-#include <stdlib.h>
+#include <stdlib.h> // Include this header for environ
 
 /**
+ * // Include this header for environ
  * shell_env -the program that prints environmental variables
  * Returreturns 0 on succesful execution
  */
