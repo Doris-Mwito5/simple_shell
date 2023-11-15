d68da535b986de3ad13e6f414953cafdd4472f86
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-11-15 02:54:52 -0800

    value.c added

---
+++ value.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+
+/**
+ * shell_setenv - a program that set the environmental variable
+ * @args: passed arguments
+ * Return:returns 0 on succesful execution
+ */
+int shell_setenv(char **args)
+{
+	char *name, *value;
+
+	if (args[1] == NULL || args[2] == NULL)
+	{
+		_puterror("Usage: setenv VARIABLE VALUE\n");
+		return (-1);
+	}
+
+	name = args[1];
+	value = args[2];
+
+	if (setenv(name, value, 1) != 0)
+	{
+		_puterror("setenv");
+		return (-1);
+	}
+	return (0);
+}
+
+/**
+ * shell_unsetenv - a program to unset an environmental variable
+ * @args: passed arguments
+ * Return: returns 0 on succesful execution
+ */
+int shell_unsetenv(char **args)
+{
+	char *name;
+
+	if (args[1] == NULL)
+	{
+		_puterror("Usage: unsetenv VARIABLE\n");
+		return (-1);
+	}
+
+	name = args[1];
+
+	if (unsetenv(name) != 0)
+	{
+		_puterror("unsetenv");
+	}
+	return (0);
+}
