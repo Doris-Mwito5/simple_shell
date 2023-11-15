a7d785578570fae12e56f92087ba588aaef1a843
Doris-Mwito5 <kawiramwito3@gmail.com>
2023-11-15 11:45:32 -0800

    update

--- commands.h
+++ commands.h
@@ -15,22 +15,21 @@
 
 extern char **environ;
 
-/* execute.c */
+
 void execute(char *commands, char **arguments, general_t *info, char *buff);
 int current_directory(char *cmd, char **arguments, char *buff,
 		general_t *info);
 
-/* permissions.c */
+
 int is_executable(char *filename);
 int is_file(char *command);
 
-/* environment.c */
+
 char *_getenv(const char *name);
 char *which(char *filename, general_t *info);
 void is_current_path(char *path, general_t *info);
 void get_full_env(void);
 
-/* commands.c */
 void analyze(char **arguments, general_t *info, char *buff);
 
 #endif
